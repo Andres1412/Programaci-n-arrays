04ccb3bead4eb3d79790a8ed267dffbc5415896f
Andrés Rocher Escriva <anroesfloridargmail.com>
2020-03-06 10:58:29 +0100

    Añadido Ejercicio 3

---
+++ Pares/src/pares/Pares.java
@@ -0,0 +1,19 @@
+package pares;
+
+public class Pares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] Pares=new int[20];
+		
+		int cont=0;
+		
+		for(int i=0; i<20; i++) {
+			cont+=2;
+			System.out.println("Número par: "+cont);
+		}
+		
+	}
+
+}
